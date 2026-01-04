d96f6724f68fa2a62b9f52bbfba02fd4669ab308
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-03 19:38:17 -0800

    Time: 3 ms (41.18%), Space: 12.4 MB (86.12%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        def backtracking(start, pairs):
+            if sum(pairs) is not n and len(pairs) == k:
+                return
+            if sum(pairs) > n or len(pairs) > k:
+                return
+            if sum(pairs) == n and len(pairs) == k:
+                print("Hello")
+                result.append(pairs[:])
+                return     
+            for i in range(start, 10):
+                if (len(pairs) + (10-start)) < k:
+                    return
+                pairs.append(i)
+                # print(pairs)
+                backtracking(i + 1, pairs)
+                pairs.pop()
+                # print(pairs)
+        j = 0
+        result = []
+        backtracking(1, [])
+        print("Hello2")
+        print(result)
+        return result
+
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        
