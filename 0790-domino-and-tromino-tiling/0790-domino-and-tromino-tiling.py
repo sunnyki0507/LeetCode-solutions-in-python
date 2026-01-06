31be52c8a36bbbc0a163c61c057864054e5b3ee1
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-05 16:30:49 -0800

    Time: 0 ms (100%), Space: 12.4 MB (93.84%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -11,11 +11,7 @@ class Solution(object):
         dp[2] = 2
         dp[3] = 5
         for i in range(4, n+1):
-            dp[i] = dp[i-1] * 2 +dp[i-3]
-        
-        # if dp[n] > mod:
-        #     return dp[n]%mod
-        # else: 
+            dp[i] = dp[i-1] * 2 +dp[i-3] 
         return dp[n] % mod
         # if n == 1:
         #     return 1
