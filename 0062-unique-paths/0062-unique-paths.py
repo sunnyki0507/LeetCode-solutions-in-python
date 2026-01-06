c03de48603b6f6d9990413cab114d1056a567c7f
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-06 10:56:52 -0800

    Time: 0 ms (100%), Space: 12.5 MB (61.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,7 +4,6 @@ class Solution(object):
         for i in range(2, m+1):
             for j in range(2, n+1):
                 grid[i][j] = grid[i-1][j] + grid[i][j-1]
-                # print(grid[i][j])
         return grid[m][n]
         """
         :type m: int
