6202bddcebc0ca7c338e3362d85876dfeffd7c8a
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-14 16:14:36 -0800

    Time: 134 ms (5.24%), Space: 23 MB (19.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,15 +1,38 @@
 class Solution(object):
     def rotate(self, nums, k):
-        left = k % len(nums)
-        print(left)
-        if left<(len(nums)/2):
-            for i in range(left):
-                add = nums.pop()
-                nums.insert(0, add)
-        else:
-            for i in range(len(nums) - left):
-                add = nums.pop(0)
-                nums.append(add)
+        l = k % len(nums)
+        if l < 1:
+            return
+        nums.reverse()
+        for i in range(int(l/2)):
+            print(int(k/2))
+            first = nums[i]
+            last = nums[l-1-i]
+            nums[i] = last
+            nums[l-i-1] = first
+        j = 1
+        for i in range(l, ((len(nums)-l)/2)+l):
+            print(i)
+            first = nums[i]
+            last = nums[len(nums)-j]
+            nums[i] = last
+            nums[len(nums)-j] = first
+            j += 1
+
+
+
+
+
+        # left = k % len(nums)
+        # print(left)
+        # if left<(len(nums)/2):
+        #     for i in range(left):
+        #         add = nums.pop()
+        #         nums.insert(0, add)
+        # else:
+        #     for i in range(len(nums) - left):
+        #         add = nums.pop(0)
+        #         nums.append(add)
 
 
         """
