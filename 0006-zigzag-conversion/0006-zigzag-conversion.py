68067a88a69f117eeb3fb6a8c3973b9a8b437996
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-20 21:31:49 -0800

    Time: 28 ms (16.14%), Space: 12.7 MB (21.1%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        row = [[] for _ in range(numRows)]
+        idx = -1
+        add = 1
+        one = False
+        if numRows == 1:
+            add = 0
+            one = True
+        for i in s:
+            idx += add
+            if idx == 0 and not one:
+                add = 1
+            if idx == (numRows - 1) and not one:
+                add = -1
+            row[idx].append(i)
+
+        return "".join("".join(inner) for inner in row)
+
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
